58eaf8253b1c563cd4db873d0ca70061261c3749
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-12-28 10:35:06 +0100

    added PaginationObjectMother and related serialization tests #minor

--- src/test/kotlin/de/hennihaus/bamdatamodel/testutils/JacksonTestUtil.kt
+++ src/test/kotlin/de/hennihaus/bamdatamodel/testutils/JacksonTestUtil.kt
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
-fun bamObjectMapper(): ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()).configure(
-    SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false
+fun bamObjectMapper(): ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()).disable(
+    SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 )
